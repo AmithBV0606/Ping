3c231fd5d66ebe0cef7f6d892025fb9fea48ec08
Amith B V <amithrao0606@gmail.com>
2025-07-14 12:05:38 +0530

    Minor UI tweek.

--- frontend/src/components/ui/tailwind-button.tsx
+++ frontend/src/components/ui/tailwind-button.tsx
@@ -11,7 +11,7 @@ export default function TailwindButton({
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
 
-      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-4 ring-1 ring-white/10 ">
+      <div className="relative flex items-center z-10 rounded-full bg-zinc-950 py-1 pl-4 pr-2 ring-1 ring-white/10 ">
         {children}
 
         <svg
