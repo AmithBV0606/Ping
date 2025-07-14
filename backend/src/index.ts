e07d36a8690a885169da8c1de0de3d95b794bc2d
Amith B V <amithrao0606@gmail.com>
2025-07-14 16:25:16 +0530

    Installed and configured cors.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+dotenv.config();
+
+// Middlewares
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express! Amittt");
