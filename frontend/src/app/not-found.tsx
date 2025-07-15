f39d813ee43ff253853f2411ae4ca9da675bb2f8
Amith B V <amithrao0606@gmail.com>
2025-07-15 08:02:38 +0530

    Created a custom page not found component using the "Fuzzy Text" compoent from React-Bits.

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import FuzzyText from "@/components/general-ui/fuzzy-text";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { useSession } from "next-auth/react";
+
+export default function NotFound() {
+  const { status } = useSession();
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={0.5}
+        enableHover={true}
+        fontSize={250}
+        fontWeight={"950"}
+      >
+        404
+      </FuzzyText>
+
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={0.5}
+        enableHover={true}
+        fontSize={60}
+      >
+        Page Not Found!
+      </FuzzyText>
+
+      <Link
+        href={status === "authenticated" ? "/dashboard" : "/"}
+        className="mt-8 cursor-pointer"
+      >
+        <Button
+          variant={"outline"}
+          className="flex items-center gap-2 cursor-pointer"
+        >
+          <ArrowLeft />
+          <span>Back to Home</span>
+        </Button>
+      </Link>
+    </div>
+  );
+}
