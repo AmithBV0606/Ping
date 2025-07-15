da5d7e5e7723f2eab33610aec6550fb28a8e00fe
Amith B V <amithrao0606@gmail.com>
2025-07-15 17:39:54 +0530

    Created a Dashboard page using the "Customizable sidebar" template from Shadcn UI.

---
+++ frontend/src/components/dashboard/app-sidebar.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+//   SidebarRail,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import Link from "next/link";
+import Image from "next/image";
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "All Chats",
+      url: "#",
+    },
+    {
+      title: "Building Your Application",
+      url: "#",
+    },
+    {
+      title: "API Reference",
+      url: "#",
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="floating" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="#">
+                <div className="bg-gray-800 text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg p-1">
+                  <Image
+                    src={"/New-Logo.png"}
+                    alt="Ping-Logo"
+                    height={30}
+                    width={30}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2 leading-none">
+                  <span className="font-medium text-xl">Ping</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-2">
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title} defaultChecked>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <NavUser
+          user={{
+            name: "Amith",
+            email: "amithrao0606@gmail.com",
+            avatar: "https://github.com/shadcn.png",
+          }}
+        />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
