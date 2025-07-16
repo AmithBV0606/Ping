1364a06a086807a006755c8bd43ffb9acd678ae5
Amith B V <amithrao0606@gmail.com>
2025-07-16 07:18:36 +0530

    Added a new schema model called "GroupUsers" in order to store the info of all the users who joins a specific chat room.

---
+++ backend/prisma/migrations/20250716014628_group_users/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "group_users" (
+    "id" SERIAL NOT NULL,
+    "group_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "group_users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "group_users" ADD CONSTRAINT "group_users_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "chat_groups"("id") ON DELETE CASCADE ON UPDATE CASCADE;
