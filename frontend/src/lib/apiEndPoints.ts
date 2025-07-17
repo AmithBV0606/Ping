b68298da3b6c89bf6148766bd77b800917c48a61
Amith B V <amithrao0606@gmail.com>
2025-07-17 08:46:55 +0530

    Added the endpoint "/chat-group" for the easy access of Endpoints.

--- frontend/src/lib/apiEndPoints.ts
+++ frontend/src/lib/apiEndPoints.ts
@@ -2,4 +2,9 @@ import Env from "./env";
 
 export const BASE_URL = Env.BACKEND_URL;
 export const API_URL = BASE_URL + "/api";
+
+// Auth Urls :
 export const LOGIN_URL = API_URL + "/auth/login";
+
+// Chat Group Urls :
+export const CHAT_GROUP_URL = API_URL + "/chat-group"
