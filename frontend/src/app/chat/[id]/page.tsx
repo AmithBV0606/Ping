4a484c357e664c6f3d22fdbfa0dd28fd4b3a6634
Amith B V <amithrao0606@gmail.com>
2025-07-20 19:42:32 +0530

    Installed uuid and created a socket.io connection from the chat-base component.

---
+++ frontend/src/app/chat/[id]/page.tsx
@@ -0,0 +1,18 @@
+import ChatBase from "@/components/chats/chat-base";
+import React from "react";
+
+export default async function ChatsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <div>
+      <h1>Hello I am chat!!</h1>
+      <p className="text-2xl">ID : {id}</p>
+      <ChatBase />
+    </div>
+  );
+}
