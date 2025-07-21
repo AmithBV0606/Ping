3eb683e147c4b79696f8d6dc1c5616ae2b09940f
Amith B V <amithrao0606@gmail.com>
2025-07-21 18:32:03 +0530

    Method to receive the message on the backend sent from the frontend. Also disabled the react strict mode.

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -7,6 +7,12 @@ export function setupSocket(io: Server) {
   io.on("connection", (socket) => {
     console.log("The socket is connected..", socket.id);
 
+    // To receive the data coming from the client side :
+    socket.on("message", (data) => {
+      console.log("Server side message : ", data);
+      socket.emit("message", data);
+    });
+
     // Defining what happens when the socket is disconnected :
     io.on("disconnect", () => {
       console.log("A user disconnected.", socket.id);
