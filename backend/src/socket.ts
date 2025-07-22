26225beb9b74e3497a78520711866e9dd7ee65f4
Amith B V <amithrao0606@gmail.com>
2025-07-22 13:36:53 +0530

    Using the "socket.room" i.e "groupId", we're connecting the user to the specified group/room and his messages will be broadcasted to the users present in that group.

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -6,7 +6,7 @@ import { CustomSocket } from "./types";
 export function setupSocket(io: Server) {
   // Middlewares :
   io.use((socket: CustomSocket, next) => {
-    const room = socket.handshake.auth.room;
+    const room = socket.handshake.auth.room || socket.handshake.headers.room;
 
     if (!room) {
       return next(new Error("Invalid room!!"));
@@ -16,15 +16,23 @@ export function setupSocket(io: Server) {
     next();
   });
 
+  // ________________________________________________________________________________________________
+
   // Defining what happens when the socket is connected :
-  io.on("connection", (socket) => {
-    console.log("The socket is connected..", socket.id);
+  io.on("connection", (socket: CustomSocket) => {
+    // Join the user to the room :
+    socket.join(socket.room!);
 
     // To receive the data coming from the client side :
     socket.on("message", (data) => {
       console.log("Server side message : ", data);
-      socket.broadcast.emit("message", data);
+
+      // socket.broadcast.emit("message", data);
+
+      io.to(socket.room!).emit("message", data); // This prevents users from broadcasting the messages to all the group, and sends the message only to the group/room the user is connected to.
     });
+
+    // _______________________________________________________________________________________________
 
     // Defining what happens when the socket is disconnected :
     io.on("disconnect", () => {
