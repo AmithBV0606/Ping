34202c0feee3b432ac39644c1a687adc82ce47de
Amith B V <amithrao0606@gmail.com>
2025-07-22 11:51:07 +0530

    Created a middleware ot receive the socket.auth object sent from the frontend and inserted the "socket.auth.room" to the custom socket object.

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,8 +1,21 @@
 // Initializing Socket Io :
 
 import { Server } from "socket.io";
+import { CustomSocket } from "./types";
 
 export function setupSocket(io: Server) {
+  // Middlewares :
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room;
+
+    if (!room) {
+      return next(new Error("Invalid room!!"));
+    }
+
+    socket.room = room;
+    next();
+  });
+
   // Defining what happens when the socket is connected :
   io.on("connection", (socket) => {
     console.log("The socket is connected..", socket.id);
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,7 +1,13 @@
+import { Socket } from "socket.io";
+
 export interface LoginPayloadType {
   name: string;
   email: string;
   provider: string;
   oauth_id: string;
   image?: string;
+}
+
+export interface CustomSocket extends Socket {
+  room?: string;
 }
